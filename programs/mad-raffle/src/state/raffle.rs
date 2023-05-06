ced5bbd4800a5c69665fe7b5d1e2e41b47dbc7a6
amilz.sol <85324096+amilz@users.noreply.github.com>
2023-05-05 20:19:16 -0700

    Update raffle.rs

--- programs/mad-raffle/src/state/raffle.rs
+++ programs/mad-raffle/src/state/raffle.rs
@@ -1,9 +1,29 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, InstructionData};
+use solana_program::{pubkey::Pubkey, instruction::Instruction, system_program};
+use crate::{id::ID, constants::{RAFFLE_SEED}};
 
 #[account]
 pub struct RaffleTracker {
     pub current_raffle: u64,
     pub bump: u8,
+}
+
+impl RaffleTracker {
+    pub const CURRENT_RAFFLE_OFFSET: u64 = 8;
+    pub const CURRENT_RAFFLE_BYTES: u64 = 8;
+    pub fn next_raffle_pda(&self) -> (Pubkey, u8) {
+        let (next_raffle_pda, next_raffle_bump) = Pubkey::find_program_address(
+            &[
+                RAFFLE_SEED.as_ref(),
+                &(self.current_raffle + 1 as u64).to_le_bytes(),
+            ],
+            &ID,
+        );
+        (next_raffle_pda, next_raffle_bump)
+    }
+    pub fn increment(&mut self) {
+        self.current_raffle += 1;
+    }
 }
 
 #[account]
@@ -24,6 +44,7 @@ pub struct TicketHolder {
 }
 
 impl Raffle {
+    const RAFFLE_VERSION:u8 = 1;
     pub fn get_space(ticket_holder_count: usize) -> usize {
         8 + // discriminator
         1 + // version
@@ -34,6 +55,54 @@ impl Raffle {
         (TicketHolder::get_space() * (ticket_holder_count)) + // tickets
         8 + // start time
         8 // end time
+    }
+    pub fn initialize(&mut self, raffle_id: u64, bump: u8) {
+        self.id = raffle_id;
+        self.active = true;
+        self.start_time = Clock::get().unwrap().unix_timestamp;
+        self.tickets = Vec::new();
+        self.end_time = 0;
+        self.version = Raffle::RAFFLE_VERSION;
+        self.bump = bump;
+    }
+    pub fn end_raffle(&mut self) {
+        self.active = false;
+        self.end_time = Clock::get().unwrap().unix_timestamp;
+    }
+    pub fn buy_ticket(&mut self, buyer: &Pubkey) {
+        match self
+            .tickets
+            .iter_mut()
+            .find(|ticket_holder| ticket_holder.user == *buyer)
+        {
+            Some(ticket_holder) => ticket_holder.qty += 1,
+            None => self.tickets.push(TicketHolder {
+                user: *buyer,
+                qty: 1,
+            }),
+        }
+    }
+    pub fn new_raffle_instruction(
+        next_raffle_pda: &Pubkey,
+        tracker: &AccountInfo,
+        thread: &AccountInfo,
+        thread_authority: &AccountInfo,
+        payer_pubkey: &Pubkey,
+    ) -> Instruction {
+        Instruction {
+            program_id: ID,
+            accounts: crate::accounts::NextRaffle {
+                new_raffle: *next_raffle_pda,
+                tracker: tracker.key(),
+                thread: thread.key(),
+                thread_authority: thread_authority.key(),
+                system_program: system_program::id(),
+                clockwork_program: clockwork_sdk::ID,
+                payer: *payer_pubkey,
+            }
+            .to_account_metas(Some(true)),
+            data: crate::instruction::NextRaffle {}.data(),
+        }
     }
 }
 
